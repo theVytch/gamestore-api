7845be13e9551a53aa9ca1ac86909da270746fb8
TheVytch <edu.vytch@gmail.com>
2022-01-19 18:08:20 -0300

    findAll para categoria utilizando padrao DTO

---
+++ src/main/java/com/m11h/gamestore/dto/CategoriaDto.java
@@ -0,0 +1,35 @@
+package com.m11h.gamestore.dto;
+
+import com.m11h.gamestore.domain.Categoria;
+
+import java.io.Serializable;
+
+public class CategoriaDto implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String nome;
+
+    public CategoriaDto(){}
+
+    public CategoriaDto(Categoria obj) {
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
--- src/main/java/com/m11h/gamestore/resources/CategoriaResource.java
+++ src/main/java/com/m11h/gamestore/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.m11h.gamestore.resources;
 
 import com.m11h.gamestore.domain.Categoria;
+import com.m11h.gamestore.dto.CategoriaDto;
 import com.m11h.gamestore.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -19,9 +21,10 @@ public class CategoriaResource {
     private CategoriaService categoriaService;
 
     @GetMapping
-    public ResponseEntity<List<Categoria>> findAll(){
-        List<Categoria> categorias = categoriaService.findAll();
-        return ResponseEntity.ok().body(categorias);
+    public ResponseEntity<List<CategoriaDto>> findAll(){
+        List<Categoria> listCategoria = categoriaService.findAll();
+        List<CategoriaDto> listCategoriaDto = listCategoria.stream().map(CategoriaDto::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listCategoriaDto);
     }
 
     @GetMapping("/{id}")
